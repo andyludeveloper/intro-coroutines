bd7bb49ef4b6d6b8ad70de9eaeb2471b5cc3a483
Andy <andyludeveloper@gmail.com>
2021-08-09 23:42:55 +0800

    [Request2Background] second attempt: updateResults every user has been received. Update the result when the counting number in same as repos.

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -4,17 +4,22 @@ import contributors.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos -> //#1 callback
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
         val allUsers = mutableListOf<User>()
+        val numberOfProcess = AtomicInteger()
         for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers -> //#2 callback
                 logUsers(repo, responseUsers)
-                allUsers += responseUsers.bodyList()
-                updateResults(allUsers.aggregate())
+                val users = responseUsers.bodyList()
+                allUsers += users
+                if (numberOfProcess.incrementAndGet() == repos.size) {
+                    updateResults(allUsers.aggregate())
+                }
             }
         }
     }
